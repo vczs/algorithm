edc23b75ef20606b928b2f0e929b1fbb81901f7a
vczs <hi.vincz@gmail.com>
2022-01-22 17:49:54 +0800

    sum_add

--- leetcode/两数相加/sum_add_test.go
+++ leetcode/两数相加/sum_add_test.go
@@ -13,14 +13,16 @@ func Test(t *testing.T) {
 	t.Run("两数相加:", testSumAdd)
 }
 
+// 测试两数相加
 func testSumAdd(t *testing.T) {
 	l1 := &ListNode{Val: 2}
-	add(l1, 4)
+	add(l1, 4) // l1链表添加节点
 	add(l1, 3)
 	l2 := &ListNode{Val: 5}
-	add(l2, 6)
+	add(l2, 6) // l2链表添加节点
 	add(l2, 4)
-	result := addTwoNumbers(l1, l2)
+	result := addTwoNumbers(l1, l2) // 调用我们两数相加的方法 并接收运算结果
+	// 打印结果
 	for {
 		fmt.Print(result.Val)
 		if result.Next == nil {
@@ -30,6 +32,7 @@ func testSumAdd(t *testing.T) {
 	}
 }
 
+// 链表添加元素
 func add(ln *ListNode, num int) {
 	temp := ln
 	for {
