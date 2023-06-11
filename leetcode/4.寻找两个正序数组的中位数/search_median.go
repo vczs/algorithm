15f5f284ea25b3ba638fb265b93c57ca21713829
vincenzo <vczvip@gmail.com>
2023-06-12 02:32:04 +0800

    4

--- leetcode/4.寻找两个正序数组的中位数/search_median.go
+++ leetcode/4.寻找两个正序数组的中位数/search_median.go
@@ -1,16 +1,12 @@
 package search_median
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	merge := []int{}
-	n1, n2 := 0, 0
 	len1, len2 := len(nums1), len(nums2)
+	merge := []int{}
+
+	n1, n2 := 0, 0
 	for {
-		if n1 == len1 || n2 == len2 || len1 == 0 || len2 == 0 {
+		if n1 == len1 || n2 == len2 {
 			break
 		}
 		if nums1[n1] < nums2[n2] {
@@ -21,6 +17,7 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 			n2++
 		}
 	}
+
 	if n1 < len1 {
 		merge = append(merge, nums1[n1:]...)
 	}
@@ -30,20 +27,11 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	return median(merge)
 }
 
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
 func median(slice []int) float64 {
 	length := len(slice)
-	var res float64
 	if length%2 == 0 {
-		res, _ = strconv.ParseFloat(fmt.Sprintf("%.5f", float64(slice[length/2-1]+slice[length/2])/2), 64)
+		return float64(slice[length/2-1]+slice[length/2]) / 2
 	} else {
-		res, _ = strconv.ParseFloat(fmt.Sprintf("%.5f", float64(slice[length/2])), 64)
+		return float64(slice[length/2])
 	}
-	return res
 }
